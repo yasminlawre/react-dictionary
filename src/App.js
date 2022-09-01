4e7bb10460696d58664d0c69b832b39fe41d7c12
yasminlawre <yasminlawre111@gmail.com>
2022-09-01 14:50:38 +0100

    styled the application

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header text-center">📖Dictionary</header>
+        <header className="App-header text-center">📖</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunrise" />
         </main>
         <footer className="App-footer">
           <small>Coded by Yasmin</small>
